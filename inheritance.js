3417b4e41d982445b15445d8b7adfbacd4a00ac4
ZerinMia <zerinmia68@gmail.com>
2022-02-24 02:31:28 +0600

    good-night

--- inheritance.js
+++ inheritance.js
@@ -1,4 +1,4 @@
-class support {
+class TeamMember {
     name;
     designation = 'Support Web Dev';
     address = 'BD';
@@ -6,38 +6,48 @@ class support {
         this.name = name;
         this.address = address;
     }
+
+}
+
+class Support extends TeamMember {
+    groupSupporTime;
+    designation = 'Support Web Dev';
+    constructor(name, address, time) {
+        super(name, address)
+        this.groupSupporTime = time;
+    }
     startSession() {
-        console.log(this.name, 'start a support session')
+        console.log(this.name, 'start a support session');
     }
 }
 
-
-class studentCare {
-    name;
-    designation = 'Student Care Web Dev';
-    address = 'BD';
-    constructor(name, address) {
-        this.name = name;
-        this.address = address;
-    }
+class studentCare extends TeamMember {
+    designation = 'Care Web Dev';
     buildARutin(Student) {
         console.log(this.name, 'Build a routin', Student)
     }
 }
 
 
-class NeptuneDev {
-    name;
-    designation = 'Student Care Web Dev';
-    address = 'BD';
-    constructor(name, address) {
-        this.name = name;
-        this.address = address;
+class NeptuneDev extends TeamMember {
+    codeEditor;
+    designation = 'Neptune App Dev';
+    constructor(name, address, editor) {
+        super(name, address);
+        this.codeEditor = editor;
     }
     releaseApp(version) {
         console.log(this.name, 'release app version', version)
     }
 }
 
+const aamir = new support('Aamir Khan', 'BD', 11);
+const salman = new support('Salman Khan', 'Mombai', 4);
+const sarukh = new support('Sarukh Khan', 'Dubai', 9);
+const akshay = new support('Akshay Kumar', 'Kolkata', 11);
+
 const alia = new studentCare('Alia Bhatt', 'Mumbai');
-console.log(alia)
+const ash = new NeptuneDev('Ash', 'Mumbai', 'Android Srudio');
+ash.releaseApp('1.4.5');
+console.log(ash.name);
+
